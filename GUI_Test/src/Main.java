1e707c85001536edbc2816086567220ddbdaf3c9
Arthur Paul Meteng <arthur.meteng@yahoo.com>
2018-04-10 15:54:55 +0200

    add switch between 2 simple scenes

--- GUI_Test/src/Main.java
+++ GUI_Test/src/Main.java
@@ -3,12 +3,16 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	
-	Button button;
+	Button button1, button2;
+	Stage window;
+	Scene scene1, scene2;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -17,9 +21,13 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("My GUI");
-		button = new Button("Click Me!!!");
-		
+		window= primaryStage;
+		Label label1 = new Label("First Scene!!!");
+		window.setTitle("My first window");
+		button1 = new Button("Go to scene 2!!!");
+		button2 = new Button("Go to scene 1!!!");
+		button1.setOnAction(e->window.setScene(scene2));
+		button2.setOnAction(e->window.setScene(scene1));
 		/**for handling a button click the method is provided in "this"( the handle method)
 		
 		button.setOnAction(this);
@@ -42,20 +50,17 @@ public class Main extends Application {
 					System.out.println("Super");
 				}
 			}
-		});**/
+		});**/ 
 		
-		// with lambda expression
-		button.setOnAction(e-> {
-			System.out.println("Super");
-			System.out.println("aaa");
-		}); 
+		StackPane layout2 = new StackPane();
+		VBox layout1 = new VBox();
+		layout1.getChildren().addAll(label1, button1);
+		layout2.getChildren().add(button2);
 		
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
-		
-		Scene scene = new Scene(layout, 300, 250);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		scene1 = new Scene(layout1, 300, 250);
+		scene2 = new Scene(layout2, 300, 250);
+		window.setScene(scene1);
+		window.show();
 	}
 
 	
