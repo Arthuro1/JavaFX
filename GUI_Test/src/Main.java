f1bda8e4834dc931b8a268f9365de039b9e89f24
Arthur Paul Meteng <arthur.meteng@yahoo.com>
2018-04-10 14:54:15 +0200

    Basic stage with basic scene.
    Click event is handled with a lambda function.

---
+++ GUI_Test/src/Main.java
@@ -0,0 +1,63 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	Button button;
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("My GUI");
+		button = new Button("Click Me!!!");
+		
+		/**for handling a button click the method is provided in "this"( the handle method)
+		
+		button.setOnAction(this);
+		
+		@Override
+		public void handle(ActionEvent event) {
+			if(event.getSource()==button) {
+				System.out.println("Super");
+			}
+		}**/
+		
+		/**or with the compact form
+		 
+		button.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				
+				if(event.getSource()==button) {
+					System.out.println("Super");
+				}
+			}
+		});**/
+		
+		// with lambda expression
+		button.setOnAction(e-> {
+			System.out.println("Super");
+			System.out.println("ehhh");
+		}); 
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		
+		Scene scene = new Scene(layout, 300, 250);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	
+
+}
